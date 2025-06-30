dafb2b48757d44f9be4951f44906a6332e21f658
james-paul25 <jamespaul.dacaldacal25@gmail.com>
2025-06-30 16:04:19 +0800

    update

--- src/Components/Future.js
+++ src/Components/Future.js
@@ -1,9 +1,18 @@
 import React from 'react';
+import Typewriter from  'typewriter-effect';
 
 const Future = () => {
     return (
-        <div className='AboutPage'>
-        <h1 className='AboutTextHeading'>Soon...</h1>
+       <div className="TypeEffect">
+        <Typewriter
+            options={{
+            strings: ['Soon...', 'Resume Under Construction', 'Check Back Later', 'Thank You for Your Patience'],
+            autoStart: true,
+            loop: true,
+            delay: 70,
+            deleteSpeed:20,
+            }}
+        />
         </div>
     );
 }
