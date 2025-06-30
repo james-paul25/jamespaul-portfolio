6899ddabfd2e50f1470cdf1214293ab5ed0a59ff
james-paul25 <jamespaul.dacaldacal25@gmail.com>
2025-06-30 14:18:01 +0800

    update Footer

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,18 +1,17 @@
 import React from 'react';
 import { FaGithub, FaLinkedin, FaInstagram, FaFacebook } from "react-icons/fa";
 import { GrMail } from "react-icons/gr";
-import { SiLeetcode } from "react-icons/si";
 
 const Footer = () => {
   return (
     <footer>
-      <h4>Developed by Devansh Sahni</h4>
-      <h4>Copyright &copy; 2023 DS</h4>
+      <h4>Copyright &copy; 2025 JAP</h4>
       <div className='footerLinks'>
-        <a href="https://github.com/james-paul25" target='_blank'><FaGithub/></a>
-        <a href="www.linkedin.com/in/james-paul-b-dacaldacal-8843792a1" target='_blank'><FaLinkedin/></a>
-        <a href='mailTo:dacaldacaljamespaul@gmail.com' target='_blank'><GrMail/></a>
-        <a href="https://leetcode.com/sahni1403/" target="_blank"><SiLeetcode/></a>
+        <a href="https://github.com/james-paul25" target='_blank' rel="noopener noreferrer"><FaGithub/></a>
+        <a href="www.linkedin.com/in/james-paul-b-dacaldacal-8843792a1" target='_blank' rel="noopener noreferrer"><FaLinkedin/></a>
+        <a href='mailTo:dacaldacaljamespaul@gmail.com' target='_blank' rel="noopener noreferrer"><GrMail/></a>
+        <a href='https://www.facebook.com/friyayy404' target='_blank' rel="noopener noreferrer"><FaFacebook/></a>
+        <a href='https://www.instagram.com/_friyay404/' target='_blank' rel="noopener noreferrer"><FaInstagram/></a>
       </div>
     </footer>
   )
